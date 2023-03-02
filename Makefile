853fd65095205db00f5f4365d8527798debe1d7d
mVenez <veneziano.matteo@orange.fr>
2023-03-02 11:48:00 +0100

    Moité methodes terminées, marchent bien
    Makefile qui marche, reponse ajoutée

--- Makefile
+++ Makefile
@@ -8,3 +8,7 @@ all: testVecteur3D
 testVecteur3D: testVecteur3D.o Vecteur3D.o
 Vecteur3D.o: Vecteur3D.cc
 testVecteur3D.o: testVecteur3D.cc Vecteur3D.h
+
+clean: 
+	rm -f *.o
+	rm testVecteur3D
--- Vecteur3D.cc
+++ Vecteur3D.cc
@@ -2,20 +2,14 @@
 #include "Vecteur3D.h"
 using namespace std;
 
-class Vecteur3D{
-public:
-    void set_coord(int coordinate, double value) {
-        if (coordinate == 0) {x = value;}
-        else if (coordinate == 1) {y = value;}
-        else if (coordinate == 2) {z = value;}
-        else {cout << "La coordonnée choisie n'existe pas" << endl;}
-    }
-    void affiche() {cout << x << ' ' << y << ' ' << z << endl;};
-    bool compare(Vecteur3D v) {
-        return (x == v.x and y == v.y and z == v.z);
-    }
-private:
-    double x;
-    double y;
-    double z;
-};
+void Vecteur3D::set_coord(int coordinate, double value) {
+    if (coordinate == 0) {x = value;}
+    else if (coordinate == 1) {y = value;}
+    else if (coordinate == 2) {z = value;}
+    else {cout << "La coordonnée choisie n'existe pas" << endl;}
+}
+void Vecteur3D::affiche() {cout << x << ' ' << y << ' ' << z << endl;};
+bool Vecteur3D::compare(Vecteur3D v) {
+    return (x == v.x and y == v.y and z == v.z);
+}
+
