7dc0be2feb71e5dfdee4af5c4bf4876019986cf1
Malekpe <backtomexico@pm.me>
2023-03-23 09:48:58 +0100

    encore test masse

--- testMasse.cc
+++ testMasse.cc
@@ -13,13 +13,30 @@ void test(){
     cout << masse2 << endl;
 
     // test getters
+    cout << endl;
     cout << "Test getters" << endl;
     cout << "masse : " << masse1.masse() << endl;
     cout << "coefficient_frottement : " << masse1.coefficient_frottement() << endl;
     cout << "position : " << masse1.position() << endl;
     cout << "vitesse : " << masse1.vitesse() << endl;
     cout << "force_subie : " << masse1.force_subie() << endl;
-    
+
+    // test ajoute_force() 
+    cout << endl;
+    cout << "force_subie : " << masse1.force_subie() << endl;
+    cout << "Test ajoute_force()" << endl;
+    masse1.ajoute_force(Vecteur3D(1,2,3));
+    cout << "force_subie : " << masse1.force_subie() << endl;
+    // test ajoute_force() opposée
+    cout << "Test ajoute_force() opposée";
+    masse1.ajoute_force(Vecteur3D(-1,-2,-3));
+    cout << "force_subie : " << masse1.force_subie() << endl;
+
+    // test mise_a_jour_forces()
+    cout << endl;
+    cout << "Test mise_a_jour_forces()" << endl;
+    // coder le test avec les ressorts
+
 }
 
 int main() {
