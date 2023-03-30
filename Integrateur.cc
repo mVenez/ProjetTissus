98bc8df291847e7f8d5701f38a765d5e12ed7438
Malekpe <backtomexico@pm.me>
2023-03-30 10:52:45 +0200

    test integrateur 1

--- Integrateur.cc
+++ Integrateur.cc
@@ -13,9 +13,9 @@ Integrateur::Integrateur(double dt) : dt_(dt) {
 }
 
 void Integrateur::evolue(Masse& masse) const {
+    masse.mise_a_jour_forces();
     masse.set_position(masse.position() + (dt_ * masse.vitesse()));
     masse.set_vitesse(masse.vitesse() + (dt_ * masse.acceleration()));
-    masse.mise_a_jour_forces();
 }
 
 //class IntegrateurEulerCromer
@@ -25,7 +25,7 @@ IntegrateurEulerCromer::IntegrateurEulerCromer(double dt) : Integrateur(dt) {}
 
 
 void IntegrateurEulerCromer::evolue(Masse& masse) const{
+    masse.mise_a_jour_forces();
     masse.set_vitesse(masse.vitesse() + (dt_ * masse.acceleration()));
     masse.set_position(masse.position() + (dt_ * masse.vitesse()));
-    masse.mise_a_jour_forces();
 }
--- testIntegrateur1.cc
+++ testIntegrateur1.cc
@@ -1,6 +1,21 @@
 #include "Integrateur.h"
 #include <iostream>
+using namespace std;
+
+void test_chute_libre(){
+    Masse masse(0.127, 0, Vecteur3D(0, 0, 1), Vecteur3D(1, 0, 2));
+    IntegrateurEulerCromer integrateur(0.01);
+    cout << "Situation de départ : " << endl << masse << endl;
+    cout << "=========================" << endl;
+    for (int i = 1; i <= 10; i++){
+        integrateur.evolue(masse);
+        cout << "Situation après " << i << " pas de temps : " << endl << masse << endl;
+        cout << "=========================" << endl;
+    }
+
+}
 
 int main() {
+    test_chute_libre();
     return 0;
 }
