62ccb73bde746a8747ed409f4b72c81f844e6052
mVenez <veneziano.matteo@orange.fr>
2023-04-03 15:09:13 +0200

    Plot testIntegrateur1

--- testIntegrateur1.cc
+++ testIntegrateur1.cc
@@ -1,8 +1,10 @@
 #include "Integrateur.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 void test_chute_libre(){
+    ofstream write("./test.txt");
     Masse masse(0.127, 0, Vecteur3D(0, 0, 1), Vecteur3D(1, 0, 2));
     IntegrateurEulerCromer integrateur(0.01);
     cout << "Situation de départ : " << endl << masse << endl;
@@ -11,6 +13,7 @@ void test_chute_libre(){
         masse.mise_a_jour_forces();
         integrateur.evolue(masse);
         cout << "Situation après " << i << " pas de temps : " << endl << masse << endl;
+        write << masse.position() << endl;
         cout << "=========================" << endl;
     }
 
