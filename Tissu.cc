bc85be62a529a426a1c59b430c99b64896817f4c
Malekpe <backtomexico@pm.me>
2023-04-06 11:14:37 +0200

    affichage tissu update

--- Tissu.cc
+++ Tissu.cc
@@ -48,12 +48,21 @@ void Tissu::check() const {
 }
 
 ostream& Tissu::affiche(ostream& out) const {
+    out << "======================" << endl;
+    out << "Tissu : " << endl;
+    out << "Les masses sont : {" << endl;
     for (auto masse : vector_masse_) {
+        out << "Masse " << &masse << " : " << endl;
         masse->affiche(out);
     }
+    out << "}" << endl;
+    out << "Les ressorts sont : {" << endl;
     for (auto ressort : vector_ressort_) {
+        out << "Ressort " << &ressort << " : " << endl;
         ressort->affiche(out, false);
     }
+    out << "}" << endl;
+    out << "======================" << endl;
     return out;
 }
 
