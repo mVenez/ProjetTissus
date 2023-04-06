7844c45ac2882d4e8884fc9d9e97b097318b8211
Malekpe <backtomexico@pm.me>
2023-04-06 10:31:46 +0200

    Tissu on … on… on… interdit la copie

--- Tissu.cc
+++ Tissu.cc
@@ -1,15 +1,24 @@
 #include <iostream>
+#include <vector>
 #include "Tissu.h"
 #include "Masse.h"
 #include "Ressort.h"
 #include "Integrateur.h"
 
-//constructeurs et destructeur
+//constructeurs
 Tissu::Tissu(Masse& masse) : vector_masse_({&masse}) {}
+
 Tissu::Tissu(std::vector<Masse*> vector_masse) : vector_masse_(vector_masse) {}
 
+
+//destructeur
+Tissu::~Tissu() {
+    for (auto ressort : vector_ressort_) delete ressort;
+}
+
+
 //methodes
-void Tissu::connecte(Masse& masse1, Masse& masse2, double k, double l0) const {
+void Tissu::connecte(Masse& masse1, Masse& masse2, double k, double l0) {
     Ressort* ressort = new Ressort(masse1, masse2, k, l0);
     vector_ressort_.push_back(ressort);
 }
--- Tissu.h
+++ Tissu.h
@@ -10,13 +10,13 @@ public:
     //constructeurs, copie, destructeur
     Tissu(Masse& masse);//constructeur pour initializer un tissu avec une seule masse puis en ajouter après
     Tissu(std::vector<Masse*> vector_masse);//constructeur auquel on passe un vecteur de masses directement
-    Tissu(const Tissu&); //copie
-    Tissu& operator=(const Tissu&); //affectation vas de paire avec la copie
+    Tissu(const Tissu&) = delete; //on interdit la copie
+    Tissu& operator=(const Tissu&) = delete; //affectation aussi interdite car copie interdite
     ~Tissu();   //faut desallouer la memoire sinon segfault
 
     //methodes
     void ajoute_masse(Masse* m);    //ajoute une masse au tissu
-    void connecte(Masse& masse1, Masse& masse2, double k, double l0) const ;    //allocation dynamique d'un nouveau ressort qui lie les deux masses
+    void connecte(Masse& masse1, Masse& masse2, double k, double l0);    //allocation dynamique d'un nouveau ressort qui lie les deux masses
     void mise_a_jour_forces() const;    //met à jour toutes les masses
     void evolue(const Integrateur& integrateur) const;  //intègre toutes les masses
 
