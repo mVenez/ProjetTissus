d94fb9f069089e9d1c971fa3cc097464686d3ba4
mVenez <veneziano.matteo@orange.fr>
2023-04-17 21:54:25 +0200

    polymorphisme evolue()

--- Objet.h
+++ Objet.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include "SupportADessin.h"
+#include "Integrateur.h"
 class Tissu;
 
 // la class objet est une class virtuelle pure qui permet à la class système de contenir des objets de type tissu ou de type contrainte (à venir) via le polymorphisme
@@ -9,8 +10,11 @@ class Objet{
     public:
         virtual void dessine_sur(SupportADessin& support) = 0;
         virtual std::ostream& affiche(std::ostream& out) const = 0;
-        //virtual std::vector<Tissu*> getTissus() const = 0;
-        //virtual std::vector<Objet*> getObjets() const = 0;
+
+        //est-ce que ces trois methodes en bas ont un sense pour une contrainte?
+        virtual void mise_a_jour_forces() const = 0;
+        virtual void evolue(const Integrateur& integrateur) const = 0;
+        virtual void check() const = 0; //celle ci est probablement pas necessaire
 };
 
 std::ostream& operator<<(std::ostream& sortie, Objet const& objet);
