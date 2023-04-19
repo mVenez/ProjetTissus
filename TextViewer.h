1ec1bb9e20c26bf80584637eef15cfe55b454979
mVenez <veneziano.matteo@orange.fr>
2023-04-19 21:41:27 +0200

    textviewer.h presque pret

--- TextViewer.h
+++ TextViewer.h
@@ -9,14 +9,13 @@ public:
 
     TextViewer(TextViewer const&)            = delete; 
     TextViewer& operator=(TextViewer const&) = delete;
-    // mais on peut les déplacer
     TextViewer(TextViewer&&)            = default; 
     TextViewer& operator=(TextViewer&&) = default;
 
     virtual void dessine(Tissu const&) = 0;
     virtual void dessine(Systeme const&) = 0;
     virtual void dessine(Masse const&) = 0;
-    
+
 private:
     std::ostream& flot;
 
