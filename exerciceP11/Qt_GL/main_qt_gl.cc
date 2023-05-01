6a2274688dfe50935dcf84dafe1bfb1eddf02499
mVenez <veneziano.matteo@orange.fr>
2023-05-01 11:05:18 +0200

    Update main_qt_gl.cc

--- exerciceP11/Qt_GL/main_qt_gl.cc
+++ exerciceP11/Qt_GL/main_qt_gl.cc
@@ -29,15 +29,15 @@ int main(int argc, char* argv[])
     Systeme systeme(tissu1);*/
     
 
-    vector<Vecteur3D> liste_position = {Vecteur3D(0, 0, 0), Vecteur3D(1, 0, 0), Vecteur3D(2, 0, 0), Vecteur3D(3, 0, 0), Vecteur3D(4, 0, 0)};
+    /*vector<Vecteur3D> liste_position = {Vecteur3D(0, 0, 0), Vecteur3D(1, 0, 0), Vecteur3D(2, 0, 0), Vecteur3D(3, 0, 0), Vecteur3D(4, 0, 0)};
     TissuChaine tissuChaine(1, 0.3, 20, 1, liste_position);
     tissuChaine.vector_masse()[0]->fixe();
     tissuChaine.vector_masse().back()->fixe();
-    Systeme systeme(tissuChaine);
+    Systeme systeme(tissuChaine);*/
 
-    /*TissuDisque tissu(1, Vecteur3D(0,0,0), Vecteur3D(3,3,3), 0.5, 0.3, 20);
-    //tissu.vector_masse().back()->fixe();
-    Systeme systeme(tissu);*/
+    TissuDisque tissu(1, Vecteur3D(0,0,0), Vecteur3D(3,3,3), 0.5, 0.3, 20);
+    tissu.vector_masse().back()->fixe();
+    Systeme systeme(tissu);
 
     //Apres cette ligne on touche pas
     GLWidget w(systeme);
