9951ac8485d2f988203766970b709a3a90503e59
mVenez <veneziano.matteo@orange.fr>
2023-05-04 08:15:32 +0200

    Crée sous-directory testClasses

--- exerciceP11/Qt_GL/main_qt_gl.cc
+++ exerciceP11/Qt_GL/main_qt_gl.cc
@@ -15,7 +15,7 @@ int main(int argc, char* argv[])
     QApplication a(argc, argv);
     //avant cette ligne on touche pas
 
-    /*Masse masseA(0.33, 0.3, Vecteur3D(0, 0, -3), Vecteur3D(0, 0, 0));
+   Masse masseA(0.33, 0.3, Vecteur3D(0, 0, -3), Vecteur3D(0, 0, 0));
     Masse masseB(1, 0.3, Vecteur3D(-0.5, 0, 0), Vecteur3D(0, 0, 0));
     Masse masseC(1, 0.3, Vecteur3D(0.5, 0, 0), Vecteur3D(0, 0, 0));
     masseB.fixe();
@@ -26,7 +26,7 @@ int main(int argc, char* argv[])
     tissu1.connecte(masseA, masseB, 0.6, 2.5);
     tissu1.connecte(masseA, masseC, 0.6, 2.5);
     tissu1.check();
-    Systeme systeme(tissu1);*/
+    Systeme systeme(tissu1);
     
 
     /*vector<Vecteur3D> liste_position = {Vecteur3D(0, 0, 0), Vecteur3D(1, 0, 0), Vecteur3D(2, 0, 0), Vecteur3D(3, 0, 0), Vecteur3D(4, 0, 0)};
@@ -35,17 +35,17 @@ int main(int argc, char* argv[])
     //tissuChaine.vector_masse().back()->fixe();
     Systeme systeme(tissuChaine);*/
 
-    TissuDisque tissu(1, Vecteur3D(0,0,0), Vecteur3D(3,3,3), 0.5, 0.3, 50);
+    /*TissuDisque tissu(1, Vecteur3D(0,0,0), Vecteur3D(3,3,3), 0.5, 0.3, 50);
     for (auto masse : tissu.vector_masse()) {
         masse->fixe();
-    }
+    }*/
 
 
     /*TissuRectangle tissu(1, Vecteur3D(3,0,0), Vecteur3D(0,2,0), Vecteur3D(0,0,0), 0.3, 3, 40);
     for (auto masse : tissu.vector_masse()) {
         masse->fixe();
     }*/
-    Systeme systeme(tissu);
+    
 
     //Apres cette ligne on touche pas
     GLWidget w(systeme);
