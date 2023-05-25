c39d3e858405f4a4e356e7b99ef5ef276467331c
Malekpe <backtomexico@pm.me>
2023-05-25 08:39:30 +0200

    passage par argument raideur k tissu compose

--- general/constantes.h
+++ general/constantes.h
@@ -5,3 +5,4 @@ extern const Vecteur3D g; //constante gravitationelle
 extern const double epsilon; //epsilon pour l’égalité des double
 extern const double dt_const; //pas de temps dt des simulation de tissu
 extern const double delta_tissu_recompose; //delta par défaut des tissu recomposé, c’est à dire l’écart en dessous duquel l’on connecte de masse avec un ressort lors de la composition de tissu
+extern const double k_ressort_de_couture; //raideur des ressorts de "couture" c’est à dire la raideur des ressorts qui relient les deux tissus lors de la composition de tissu
