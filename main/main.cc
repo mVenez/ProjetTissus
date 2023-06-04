fb96f4a1530894bd2a1ebab9b9320657a1084cc9
Malekpe <backtomexico@pm.me>
2023-06-04 11:35:09 +0200

    main final trop bien génial

--- main/main.cc
+++ main/main.cc
@@ -44,58 +44,57 @@ int main(int argc, char* argv[])
     // ================= ENCEINTE (de musique) =================
 
 
-    // ================= trampoline1 avec implusion vers le bas = g =================
-    Vecteur3D position_trampoline1(15,15,0);
-    double angle_trampoline1 = M_PI/9;
-    double rayon_trampoline1 = 10;
+    // ================= TRAMPOLINE =================
+    Vecteur3D position_trampoline(20,-10,-10);
+    double angle_trampoline = M_PI/9;
+    double rayon_trampoline = 10;
     double pas = 1;
-    Vecteur3D normal_trampoline1(0,0,1);
-    TissuDisque trampoline1_base(0.4, position_trampoline1, normal_trampoline1, rayon_trampoline1, pas, 0.3, 100, true, angle_trampoline1);
-    TissuCompose trampoline1(trampoline1_base);
-    int nb_angle_tour = 2*M_PI/angle_trampoline1;
-    int nb_masse_rayon = rayon_trampoline1/pas;
-    Vecteur3D u_trampoline1;
-    if (normal_trampoline1.x() != 0) u_trampoline1 = ~(Vecteur3D(0, 1, 0) ^ normal_trampoline1);
-    else u_trampoline1 = ~(Vecteur3D(1, 0, 0) ^ normal_trampoline1);
+    Vecteur3D normal_trampoline(0,0,1);
+    TissuDisque trampoline_base(0.4, position_trampoline, normal_trampoline, rayon_trampoline, pas, 0.3, 200, true, angle_trampoline);
+    TissuCompose trampoline(trampoline_base);
+    int nb_angle_tour = 2*M_PI/angle_trampoline;
+    Vecteur3D u_trampoline;
+    if (normal_trampoline.x() != 0) u_trampoline = ~(Vecteur3D(0, 1, 0) ^ normal_trampoline);
+    else u_trampoline = ~(Vecteur3D(1, 0, 0) ^ normal_trampoline);
     for(double n = 0; n < nb_angle_tour; ++n){
-        u_trampoline1 = (cos(angle_trampoline1)*u_trampoline1 + sin(angle_trampoline1)*(normal_trampoline1^u_trampoline1));
-        TissuChaine* pied_trampoline1 = new TissuChaine(0.1, 0.6, position_trampoline1 + u_trampoline1*rayon_trampoline1, position_trampoline1 + u_trampoline1*rayon_trampoline1 + Vecteur3D(0,0,-3), 0.5, 50, true);
-        trampoline1.ajoute_tissu(*pied_trampoline1, 1, 0.01);
+        u_trampoline = (cos(angle_trampoline)*u_trampoline + sin(angle_trampoline)*(normal_trampoline^u_trampoline));
+        TissuChaine* pied_trampoline = new TissuChaine(0.1, 0.6, position_trampoline + u_trampoline*rayon_trampoline, position_trampoline + u_trampoline*rayon_trampoline + Vecteur3D(0,0,-3), 0.5, 50, true);
+        trampoline.ajoute_tissu(*pied_trampoline, 1, 0.01);
     }
-
-    // ================= ICI  =================
-    Impulsion impuls8(position_trampoline1, 30, 0, 120, 0.4*g, trampoline1_base);
-    toutes_les_contraintes.push_back(&impuls8);
-    // ================= ICI  =================
-
-    /*for(double n = 0; n < nb_masse_rayon; ++n){
-        ImpulsionSin* impuls = new ImpulsionSin(position_trampoline1, pas*n, 0, 120, normal_trampoline1*10*(nb_masse_rayon-n), 3, trampoline1_base);
-        toutes_les_contraintes.push_back(impuls);
-    }*/
-    //ImpulsionSin impuls7(position_trampoline1, 30, 0, 120, -normal_trampoline1*(g.norme()), 5, trampoline1_base);
-    //toutes_les_contraintes.push_back(&impuls7);
-    tous_les_tissus.push_back(&trampoline1);
-    // ================= trampoline1 =================
-
-
-    // ================= trampoline2 simplement avec la gravité =================
-    Vecteur3D position_trampoline2(15,-10,0);
-    double angle_trampoline2 = M_PI/9;
-    double rayon_trampoline2 = 10;
-    Vecteur3D normal_trampoline2(0,0,1);
-    TissuDisque trampoline2_base(0.4, position_trampoline2, normal_trampoline2, rayon_trampoline2, pas, 0.3, 100, true, angle_trampoline2);
-    TissuCompose trampoline2(trampoline2_base);
-    Vecteur3D u_trampoline2;
-    if (normal_trampoline2.x() != 0) u_trampoline2 = ~(Vecteur3D(0, 1, 0) ^ normal_trampoline2);
-    else u_trampoline2 = ~(Vecteur3D(1, 0, 0) ^ normal_trampoline2);
-    for(double n = 0; n < nb_angle_tour; ++n){
-        u_trampoline2 = (cos(angle_trampoline2)*u_trampoline2 + sin(angle_trampoline2)*(normal_trampoline2^u_trampoline2));
-        TissuChaine* pied_trampoline2 = new TissuChaine(0.1, 0.6, position_trampoline2 + u_trampoline2*rayon_trampoline2, position_trampoline2 + u_trampoline2*rayon_trampoline2 + Vecteur3D(0,0,-3), 0.5, 50, true);
-        trampoline2.ajoute_tissu(*pied_trampoline2, 1, 0.01);
+    for(double n = 0; n < 20; ++n){
+        Impulsion* impulsA = new Impulsion(position_trampoline, 2.5, 10*n, 10*n + 4, Vecteur3D(0, 0, -8), trampoline_base);
+        toutes_les_contraintes.push_back(impulsA);
+        Impulsion* impulsB = new Impulsion(position_trampoline, 30, 10*n + 4, 10*n + 10, Vecteur3D(0, 0, -0.7), trampoline_base);
+        toutes_les_contraintes.push_back(impulsB);
     }
-    tous_les_tissus.push_back(&trampoline2);
-    // ================= trampoline2 =================
+    tous_les_tissus.push_back(&trampoline);
+    // ================= TRAMPOLINE =================
 
+    // ================= TISSU MALMENER =================
+    TissuRectangle tissu_malmener(0.4, Vecteur3D(5,-1,3), Vecteur3D(-2,4,-1), Vecteur3D(0,15,-5), 0.4, 0.1, 1200, true);
+    for(double n = 0; n < 20; ++n){
+        Impulsion* impulsA = new Impulsion(Vecteur3D(0,0,0), 300, 20*n, 20*n + 5, Vecteur3D(10, 0, 0), tissu_malmener);
+        toutes_les_contraintes.push_back(impulsA);
+        Impulsion* impulsB = new Impulsion(Vecteur3D(0,0,0), 300, 20*n + 5, 20*n + 10, Vecteur3D(0, 0, 10), tissu_malmener);
+        toutes_les_contraintes.push_back(impulsB);
+        Impulsion* impulsC = new Impulsion(Vecteur3D(0,0,0), 300, 20*n + 10, 20*n + 15, Vecteur3D(-10, 0, 0), tissu_malmener);
+        toutes_les_contraintes.push_back(impulsC);
+        Impulsion* impulsD = new Impulsion(Vecteur3D(0,0,0), 300, 20*n + 15, 20*n + 20, Vecteur3D(0, 0, -10), tissu_malmener);
+        toutes_les_contraintes.push_back(impulsD);
+    }
+    tous_les_tissus.push_back(&tissu_malmener);
+    // ================= TISSU MALMENER =================
+
+
+    // ================= BAC À SABLE =================
+
+    // créez le tissu composé/complexe que vous voulez ici
+    // vous pouvez vous servir des exemples ci-dessus pour vous aider
+    // n'oubliez pas de l'ajouter à la liste des tissus avec tous_les_tissus.push_back(&votre_tissu);
+    // ansi que les contraintes avec toutes_les_contraintes.push_back(&votre_contrainte);
+
+
+    // ================= BAC À SABLE =================
 
 
     Systeme systeme(tous_les_tissus, toutes_les_contraintes);
